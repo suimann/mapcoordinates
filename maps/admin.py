7e1964d76b7807ba9d4c4821c5fa3972a1702311
Jan Sarstedt <jan@Laptop-Jan.(none)>
2012-08-07 10:03:01 +0200

    Daniel ist zur Datenbank hinzugefügt. Fehler in admin.py Zeile 4 behoben.

--- maps/admin.py
+++ maps/admin.py
@@ -1,8 +1,6 @@
 from maps.models import Location, Coordinate
 from django.contrib import admin
 
-class CoordinateInLine(admin.StackedInLine)
-
 class LocationAdmin(admin.ModelAdmin):
     fields=['entity_id', 'name']
 
