21eadf15c6665b9efdd15b5a3e422e268df671b6
corch1988 <dczempisz@gmail.com>
2012-08-08 16:54:04 +0200

    Hooray

--- maps/admin.py
+++ maps/admin.py
@@ -1,11 +1,6 @@
 from maps.models import Map, Location, Link
 from django.contrib import admin
 
-<<<<<<< HEAD
 admin.site.register(Map)
 admin.site.register(Location)
 admin.site.register(Link)
-=======
-admin.site.register(Location)
-admin.site.register(Coordinate)
->>>>>>> master
