02f825beb93e865d2f7b1961ae96546fafb6e4af
corch1988 <dczempisz@gmail.com>
2012-08-09 14:43:02 +0200

    Andere Struktur aber ohne Speichern

--- maps/views.py
+++ maps/views.py
@@ -4,16 +4,50 @@ from django.views.decorators.gzip import gzip_page
 from django.views.decorators.http import require_safe, require_POST
 from maps.models import Map, Location, Link
 
+#------------------ Laden der Begruessungsseite ----------------------------
 @csrf_exempt
 def index(request):
-    if request.POST:
-        print request.POST
-        map = get_object_or_404(Map, map_url=request.POST['map_name'])
-        p = Link(map_id=map.id, location_id=request.POST['location_name'], link_coordinate=request.POST['coords'])
-        p.save()
-    else:
-        print "Nix GE(H)T"
+    
     map_list = Map.objects.all().order_by('-map_name')
+
+    return render_to_response('maps/index.html', {'map_list': map_list})
+#...........................................................................
+
+#------------------ Laden der Koordinatenseite -----------------------------
+@csrf_exempt
+def choose(request):
+        
+    #Deklaration der Variablen um die Selektion beizubehalten
+    map_id = request.POST['map_id']
+    map = Map.objects.get(id=map_id)
+
+    #Holen der zu uebergebenen Standard Daten aus der Datenbank
     location_list = Location.objects.all().order_by('-location_name')
-    return render_to_response('maps/index.html', {'map_list': map_list, 'location_list': location_list})
+    coordinates_list = Link.objects.all()
+
+    return render_to_response('maps/choose_coords.html', {'map': map, 'location_list': location_list, 'coordinates_list': coordinates_list})
+#...........................................................................
+
+#----------------------Speichern der Koordinaten ---------------------------
+@csrf_exempt
+def save(request):
+
+    #Deklaration der Variablen um die Selektion beizubehalten
+    map_id = request.POST['map_id']
+    map = Map.objects.get(id=map_id)
+
+    #Holen der zu uebergebenen Standard Daten aus der Datenbank
+    location_list = Location.objects.all().order_by('-location_name')
+    coordinates_list = Link.objects.all()
+
+    return render_to_response('maps/choose_coords.html', {'map': map, 'location_list': location_list, 'coordinates_list': coordinates_list})
+    
+#...........................................................................
+
+
+
+
+
+
+
 
