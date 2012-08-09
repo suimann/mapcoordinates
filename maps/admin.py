2d788a9bca7f57ee15ab6d5cd67329e6ef6446db
Jan Sarstedt <jan@Laptop-Jan.(none)>
2012-08-09 10:31:38 +0200

    new version on master

--- maps/admin.py
+++ maps/admin.py
@@ -1,18 +1,6 @@
 from maps.models import Map, Location, Link
 from django.contrib import admin
-<<<<<<< HEAD
-    
 
-<<<<<<< HEAD
-class LocationAdmin(admin.ModelAdmin):
-    fields=['entity_id', 'name']
-
-admin.site.register(Location, LocationAdmin)
-admin.site.register(Coordinate)
-=======
-
-=======
->>>>>>> jan
 class MapAdmin(admin.ModelAdmin):
     fieldsets = [
     ('Map information', {'fields': ['map_name', 'map_url']}),    
@@ -29,13 +17,5 @@ class LinkAdmin(admin.ModelAdmin):
 
 admin.site.register(Map, MapAdmin)
 admin.site.register(Location)
-<<<<<<< HEAD
-<<<<<<< HEAD
-admin.site.register(Link)
->>>>>>> 21eadf15c6665b9efdd15b5a3e422e268df671b6
-=======
 admin.site.register(Link, LinkAdmin)
->>>>>>> jan
-=======
-admin.site.register(Link, LinkAdmin)
->>>>>>> jan
+
