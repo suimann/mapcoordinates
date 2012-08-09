e6d262855f11b5ba59b817cbe2b5b59329bee6f2
Jan Sarstedt <jan@Laptop-Jan.(none)>
2012-08-09 10:16:46 +0200

    ich weiß nicht was geändert wurde

--- maps/admin.py
+++ maps/admin.py
@@ -10,7 +10,25 @@ admin.site.register(Location, LocationAdmin)
 admin.site.register(Coordinate)
 =======
 
-admin.site.register(Map)
+class MapAdmin(admin.ModelAdmin):
+    fieldsets = [
+    ('Map information', {'fields': ['map_name', 'map_url']}),    
+    ]    
+    list_display = ('map_name', 'map_url')
+
+class LinkAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['map']}),
+        ('Location', { 'fields': ['location', 'link_coordinate'] }),    
+    ]
+    list_display = ('map', 'location', 'link_coordinate')
+    list_filter = ['map']
+
+admin.site.register(Map, MapAdmin)
 admin.site.register(Location)
+<<<<<<< HEAD
 admin.site.register(Link)
 >>>>>>> 21eadf15c6665b9efdd15b5a3e422e268df671b6
+=======
+admin.site.register(Link, LinkAdmin)
+>>>>>>> jan
